00eb5b8e5a397a686f6042403c28dcf082ca6f82
brandon121ss <cristianmentado3@gmail.com>
2025-02-05 15:45:34 -0600

    Create proyectoFinal.java

---
+++ proyectoFinal.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+
+public class proyectoFinal{
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame("Mi primer proyeto");
+        frame.setSize(500, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.setLayout(null);
+
+        JLabel etiqueta_anuncio = new JLabel("¡Hola!,");
+        JLabel etiqueta_2 = new JLabel("¡chequeo");
+        JLabel etiqueta_3 = new JLabel("¡chequeo encima 1");
+        JLabel etiqueta_4 = new JLabel("¡chequeo encima 2");
+        JButton button = new JButton("Enviar");
+        JLabel nombre_usuario = new JLabel("Nombre");
+        JTextField textField = new JTextField(20);
+
+        etiqueta_anuncio.setBounds(30, 50, 100, 30);
+        etiqueta_2.setBounds(300, 50, 200, 30);
+        etiqueta_3.setBounds(30, 90, 100, 30);
+        etiqueta_4.setBounds(300, 90, 200, 30);
+        button.setBounds(30, 90, 100, 30);
+        nombre_usuario.setBounds(50, 180, 200, 30);
+        textField.setBounds(200, 160, 200, 30);
+    }
+}
